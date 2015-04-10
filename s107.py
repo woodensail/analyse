4f1972e4a8acf896f3557277de0e4c2f2e6b64b7
woodensail <woodensail@hotmail.com>
2015-04-10 21:27:43 +0800

    Fix bugs in s107
    修改主lambda表达式, 用基于dict的switch实现替代了if else, 提高可读性。
    修复用于解释主lambda的tt函数中的bug。
    修复,将行数当成字段数的bug

--- s107.py
+++ s107.py
@@ -14,10 +14,10 @@ def analyse():
     # 该函数用于对一个Series进行合并,入参x为一个Series
     def tt(x):
         # 当x为标题时用reduce调用lcs函数求从字符串头开始的最长公共子串
-        if x == '标题':
+        if x.name == '标题':
             return reduce(lcs, x)
         # 当x为下列内容是取最后一项
-        elif x in {'日期', '粉丝数'}:
+        elif x.name in {'日期', '粉丝数'}:
             return x[-1]
         # 其他情况时对将x中所有数据合并为一组求和，并返回该组求和结果
         else:
@@ -34,8 +34,8 @@ def analyse():
     df = pd.ExcelFile(r"resource/wxgzhdata.xlsx").parse(u"3月")
     df.insert(0, '标题', df['文章标题'])
     df = df.fillna(method="pad").dropna().set_index(['文章标题']).groupby(lambda x: x[:2]).apply(lambda x: x.apply(
-        lambda y: y.groupby(lambda z: 1).sum().values[0] if y.name not in {'标题', '日期', '粉丝数'} else (
-            y[-1]) if y.name != '标题' else reduce(lcs, y))).set_index(['标题'])
+        lambda y: {1: lambda z: reduce(lcs, z), 2: lambda z: z[-1], None: lambda z: z.groupby(type).sum().values[0]}[
+            {'标题': 1, '日期': 2, '粉丝数': 2}.get(y.name)](y))).set_index(['标题'])
     # 下面为上句的另一种表达方式
     # 用fillna填充nan
     # df=df.fillna(method="pad")
@@ -47,6 +47,8 @@ def analyse():
     # df=df.groupby(lambda x: x[:2])
     # 利用函数t对每个groupBy进行合并
     # df=df.apply(t)
+    # 将各系列内文章标题的相同部分作为系列名称
+    # df=df.set_index(['标题'])
 
     # 从dataframe中取得之后计算需要的Series
     fields = [df.xs('总阅读人数', axis=1), df.xs('初次打开阅读人数', axis=1), df.xs('分享次数', axis=1), df.xs('每日增粉人数', axis=1),
@@ -56,8 +58,9 @@ def analyse():
     expressions = [('初次打开率', lambda f: f[1] / f[4]), ('分享率', lambda f: f[2] / f[0]), ('分享拉粉率', lambda f: f[3] / f[2]),
                    ('增粉速率', lambda f: f[3] / f[4]), ( '阅读涨粉率', lambda f: f[3] / f[0]),
                    ('传播涨粉率', lambda f: f[3] / (f[0] - f[1])), ('二次传播率', lambda f: (f[0] - f[1]) / f[0])]
+    # 记录现有字段数量，用于下面计算新字段插入位置以及删除旧字段。
+    old_field_count = len(df.columns)
     # 计算各字段的值，处理为percent后，依次插入datagrame中
-    old_field_count = len(df)
     for i in enumerate(expressions, start=old_field_count):
         df.insert(i[0], i[1][0], (i[1][1](fields)).apply(to_percent))
     # 转制后将原始字段去除
